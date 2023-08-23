3c9a48ed635d737783c6daee0864cfb447663ee7
Masoud Ghodrati <ghodrati.masoud@gmail.com>
2023-08-23 15:42:41 +1000

    fix: comment

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ const config =
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
+    // "@semantic-release/changelog",
     "@semantic-release/github",
     "@semantic-release/git"
   ]
